da2b1d7dd173fe518d403f9a7f9a768220495496
gloomyTofu <carie.phon@gmail.com>
2014-11-13 11:02:06 -0800

    Detect Window Height
    Adjust div height to fit in browser window

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+$(function(){
+  $('.main-section').css({ height: $(window).innerHeight() });
+  $(window).resize(function(){
+    $('.main-section').css({ height: $(window).innerHeight() });
+  });
+});
