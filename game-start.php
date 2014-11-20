632071fef7935298a0a7ae83ddf7ad605cc82b45
gloomyTofu <carie.phon@gmail.com>
2014-11-20 10:51:04 -0800

    Game Intro

---
+++ game-start.php
@@ -0,0 +1,23 @@
+<!-- Game Start -->
+<div id="game-start">
+    <div class="singleModal">
+        <div class="row">
+            <div class="small-12 small-centered text-center columns">
+                <h2><span class="waffle">Step 1: Prep</span></h2>
+            </div>
+        </div>
+         <div class="row">
+            <div class="medium-5 large-4 columns small-text-center medium-text-right margin-bottom">
+                <svg class="placeholder" width="100" height="100">
+                </svg>
+            </div>
+            <div class="small-12 small-centered medium-7 medium-uncentered columns small-text-center medium-text-left">
+                <h5 class="margin-top"><span class="waffle">Instructions</span></h5>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vel ligula vehicula, volutpat tortor fermentum, feugiat est.
+                </p>
+            </div>
+        </div>
+    </div>
+      <a class="close-reveal-modal">&#215;</a>
+</div><!--end of Game Start-->
--- index.php
+++ index.php
@@ -5,11 +5,13 @@
             <div class="row">
                 <div class="small-12 medium-7 small-centered columns">
                     <section class="text-center">
-                        <svg viewBox="4 0 198 198">
-                            <use xlink:href="images/wafflesvg.svg#waffle-svg"></use>
-                        </svg>
+                        <a class="pageLink" title="Waffle Directory" href="directory.php?v=1">
+                            <svg viewBox="4 0 198 198">
+                                <use xlink:href="images/wafflesvg.svg#waffle-svg"></use>
+                            </svg>
+                        </a>
                         <h1 class="title">
-                            <a class="pageLink" title="directory" href="directory.php?v=1"> waffle style <svg class="svgIcon" viewBox="0 0 30 25"><use xlink:href="images/svgicons.svg#right-arrow"></use></svg>
+                            <a class="pageLink" title="Waffle Directory" href="directory.php?v=1"> waffle style <svg class="svgIcon" viewBox="0 0 30 25"><use xlink:href="images/svgicons.svg#right-arrow"></use></svg>
                             </a>
                         </h1>
                     </section>
