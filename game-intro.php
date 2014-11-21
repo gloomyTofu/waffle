44043f87209fbdd3b22979edd848b2a5b943646c
gloomyTofu <carie.phon@gmail.com>
2014-11-20 16:30:47 -0800

    Game Stage

--- game-intro.php
+++ game-intro.php
@@ -1,13 +1,13 @@
 <!-- Game Start -->
 <div id="game-intro">
-    <div class="singleModal">
+    <div class="singleModal gameMode">
         <div class="row">
             <div class="small-12 small-centered text-center columns">
                 <h2><span class="waffle">Step 1: Prep</span></h2>
             </div>
         </div>
-         <div class="row padding-right padding-left">
-            <div class="medium-5 large-4 columns small-text-center medium-text-right margin-bottom">
+         <div class="row padding-right padding-left margin-bottom-2">
+            <div class="medium-5 large-4 columns small-text-center medium-text-right">
                 <svg class="placeholder" width="100" height="100">
                 </svg>
             </div>
@@ -18,7 +18,7 @@
                 </p>
                 <p>Proin quis molestie erat. Mauris tristique euismod nisi, id dignissim nisl maximus ac. Nunc sit amet nibh nisi. Nullam eget ligula libero.</p>
                 
-                <a href="game-play.php?v=1" role="button" class="pageLink button right margin-right">Start Game</a>
+                <a href="game-play.php?v=1" role="button" class="pageLink button right margin-right padding-top">Start Game</a>
             </div>
         </div>
     </div>
---
+++ game-play.php
@@ -0,0 +1,10 @@
+<?php include 'content/header.php'; ?>
+
+<div class="main-section">
+    <section class="main-content" id="game-play"> 
+        <div class="row">
+        </div>
+    </section>
+</div><!--end of main-section-->
+
+<?php include 'content/footer.php'; ?>
